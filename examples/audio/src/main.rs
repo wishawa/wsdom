1642db804c726527d5c3404fc8ceb870d4223e40
Wisha Wanichwecharungruang <wisha.wa@yandex.com>
2024-03-13 06:17:08 +0000

    add small WebAudio example

--- examples/audio/src/main.rs
+++ examples/audio/src/main.rs
@@ -20,26 +20,55 @@ async fn app(browser: Browser) {
     // create the UI
     let document = wsdom::dom::document(&browser);
     let body = document.get_body();
-    let btn = document.create_element(&"button", &wsdom::null());
-    btn.set_inner_text(&"play t-rex sound (make sure you don't have autoplay blocked)");
-    body.append_child(&btn);
+    let btn1 = document.create_element(&"button", &wsdom::undefined());
+    let btn2 = document.create_element(&"button", &wsdom::undefined());
+    btn1.set_inner_text(
+        &"play t-rex sound with Audio() (make sure you don't have autoplay blocked)",
+    );
+    btn2.set_inner_text(&"play 440 Hz with WebAudio OscillatorNode");
+    body.append_child(&btn1);
+    body.append_child(&btn2);
 
-    // make an audio
-    let audio = wsdom::dom::Audio(
+    // prepare the audio
+    let trex_audio = wsdom::dom::Audio(
         &browser,
         &Some(&"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"),
     );
+    let context = wsdom::dom::AudioContext::new(&browser, &wsdom::undefined());
+    let oscillator = context.create_oscillator();
+    oscillator.get_frequency().set_value(&440);
+    oscillator.connect_with_destinationNode_output_input(
+        &context.get_destination(),
+        &wsdom::undefined(),
+        &wsdom::undefined(),
+    );
+    context.suspend();
+    oscillator.start(&wsdom::undefined());
 
     // set up the click listener
-    let mut click_stream = {
+    let mut click_stream1 = {
         let (callback, func) = wsdom::callback::new_callback::<JsValue>(&browser);
-        btn.add_event_listener(&"click", &func, &wsdom::null());
+        btn1.add_event_listener(&"click", &func, &wsdom::null());
         callback
     };
+    let mut click_stream2 = {
+        let (callback, func) = wsdom::callback::new_callback::<JsValue>(&browser);
+        btn2.add_event_listener(&"click", &func, &wsdom::null());
+        callback
+    };
+
     // wait for click
     loop {
-        click_stream.next().await;
-        audio.set_current_time(&0.0);
-        audio.play();
+        tokio::select! {
+            _ = click_stream1.next() => {
+                trex_audio.set_current_time(&0.0);
+                trex_audio.play();
+            }
+            _ = click_stream2.next() => {
+                context.resume();
+                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                context.suspend();
+            }
+        }
     }
 }
